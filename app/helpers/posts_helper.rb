a5283e5bf101a6d84b08fe7067e1d79e2f86d105
Syamantak Mukhopadhyay <mukherjee.syamantak@gmail.com>
2011-05-06 19:30:21 +0100

    truncate post added

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,12 @@ module PostsHelper
   def popular_posts
     @popularPosts = Post.find(:all, :order => "comment_count desc", :limit => 10)
   end
+  
+  def truncate_words(text, length = 80)
+     
+    return if text == nil
+    end_string = " ..."
+    words = text.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
 end
